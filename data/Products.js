f154999d681d458656afa62b648cd4a9b99f9391
Clarice Bouwer <cbillowes@gmail.com>
2019-10-10 09:25:22 +0200

    Add products & logos

---
+++ data/Products.js
@@ -0,0 +1,82 @@
+export default [
+  {
+    title: "Business Cards",
+    image: "business-cards.png",
+    description:
+      "We will make those small quantity orders if that is what you require. You can choose a matt or gloss finish."
+  },
+  {
+    title: "Mouse Pads",
+    image: "mousepads.png",
+    description:
+      "Foam mouse pads with any design you like be it logos with company details, advertising, kiddies and adult themes."
+  },
+  {
+    title: "Your Own Design Mug",
+    image: "mugs.png",
+    description:
+      "Coffee mugs, plain white or coloured inside with anything you desire printed on the outside. In one area or all the way around the mug."
+  },
+  {
+    title: "T-Shirts, Shirts and Caps",
+    image: "tshirts.png",
+    description:
+      "Anything you want on a T-Shirt, Cap, Shirt, Pillow Case, Apron etc. Print in Vinyl, Glitter, Flock or Heat Transfer Paper."
+  },
+  {
+    title: "Standard Clock Shapes",
+    image: "clocks.png",
+    description:
+      "Clocks are made from board or fibre. They come with mechanism and hands. Print whatever you like on them. Uses 1 AA battery which is not included."
+  },
+  {
+    title: "Clock Hands",
+    image: "clockhands.png",
+    description:
+      "Hands come with your clock. We will assemble it after printing."
+  },
+  {
+    title: "Plates and Stands",
+    image: "plates.png",
+    description:
+      "Make a memory plate reminding you of a special occasion, wedding, anniversaries, holiday destinations, any photo can be printed on your plate."
+  },
+  {
+    title: "Coasters in Board & Foam",
+    image: "coasters.png",
+    description:
+      "Customise your own coasters or give them out as gifts. Photos, text, comics, animals, places of interest, almost anything can be printed on them."
+  },
+  {
+    title: "Mirrored Picture Frames",
+    image: "mirror.png",
+    description: "Have a picture of your child or partner framed by a mirror."
+  },
+  {
+    title: "Wine Bottle Holders",
+    image: "wine-bottle-holder.png",
+    description: "Perfect gift with your details printed on the front."
+  },
+  {
+    title: "Jam Jars",
+    image: "jars.png",
+    description:
+      "Do you have a pub or bar? Have your own logo, slogan or picture printed on the jar."
+  },
+  {
+    title: "Water Bottles",
+    image: "water-bottle.png",
+    description:
+      "Aluminium screw-top, with clip, water bottle iwth your personal design printed on it."
+  },
+  {
+    title: "Personalised Lighters",
+    image: "lighter.png",
+    description: "A perfect gift for a smoker or anyone who needs a light for a braai. Personalise as you desire."
+  },
+  {
+    title: "Cardboard Puzzles",
+    image: "puzzles-rect.png",
+    description: "Photos of your children or animals or places your children have visited with small and large pieces."
+  }
+];
