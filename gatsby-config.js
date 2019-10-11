69bd20621e0b4a1eca2aff1e3f3a89267e5849cb
Clarice Bouwer <cbillowes@gmail.com>
2019-10-11 09:15:40 +0200

    Remove posts filesystem source

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,13 +31,6 @@ module.exports = {
       }
     },
     {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "posts",
-        path: `${__dirname}/content/`
-      }
-    },
-    {
       resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
